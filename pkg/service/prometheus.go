bf9e530edb648e34ef071e8f52c8c009584dce22
chano <chanowang@yunify.com>
2022-05-27 19:20:27 +0800

    feat: servicemonitor

--- pkg/service/prometheus.go
+++ pkg/service/prometheus.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"time"
 
-	v1 "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned/typed/monitoring/v1"
+	monv1 "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned/typed/monitoring/v1"
+	monv1alp1 "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned/typed/monitoring/v1alpha1"
 	"github.com/prometheus/client_golang/api"
-	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	promv1cli "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
 	"github.com/tkeel-io/kit/log"
 	pb "github.com/tkeel-io/tkeel-monitor/api/prometheus/v1"
@@ -16,26 +17,34 @@ import (
 )
 
 type PrometheusService struct {
-	mc   *v1.MonitoringV1Client
-	pAPI promv1.API
+	PromNS       string
+	TKeelNS      string
+	monV1Cli     *monv1.MonitoringV1Client
+	monV1Alp1Cli *monv1alp1.MonitoringV1alpha1Client
+
+	pAPI promv1cli.API
 }
 
-func NewPrometheusService(namespace string) *PrometheusService {
+func NewPrometheusService(promNamespace, tKeelNamespace string) *PrometheusService {
 	conf, err := rest.InClusterConfig()
 	if err != nil {
 		log.Fatalf("in cluster config: %s", err)
 	}
-	mc, err := v1.NewForConfig(conf)
+	mc, err := monv1.NewForConfig(conf)
 	if err != nil {
 		log.Fatalf("new monitor client: %s", err)
 	}
-	l, err := mc.Prometheuses(namespace).List(context.TODO(), metav1.ListOptions{Limit: 1})
+	mca1, err := monv1alp1.NewForConfig(conf)
+	if err != nil {
+		log.Fatalf("new monitor client: %s", err)
+	}
+	l, err := mc.Prometheuses(promNamespace).List(context.TODO(), metav1.ListOptions{Limit: 1})
 	if err != nil {
 		log.Errorf("list prometheus: %s", err)
 		return nil
 	}
 	if len(l.Items) == 0 {
-		log.Fatalf("namespace(%s) not has prometheus", namespace)
+		log.Fatalf("promNamespace(%s) not has prometheus", promNamespace)
 	}
 	url := ""
 	p := l.Items[0]
@@ -50,27 +59,30 @@ func NewPrometheusService(namespace string) *PrometheusService {
 	if err != nil {
 		log.Fatalf("new prometheus client err: %s", err)
 	}
-	pAPI := promv1.NewAPI(pc)
+	pAPI := promv1cli.NewAPI(pc)
 	res, err := pAPI.Buildinfo(context.TODO())
 	if err != nil {
 		log.Errorf("new prometheus api build.info err: %s", err)
 	}
 	log.Debugf("prometheus build.info:\n %v", res)
 	return &PrometheusService{
-		pAPI: pAPI,
-		mc:   mc,
+		PromNS:       promNamespace,
+		TKeelNS:      tKeelNamespace,
+		pAPI:         pAPI,
+		monV1Cli:     mc,
+		monV1Alp1Cli: mca1,
 	}
 }
 
 func (s *PrometheusService) Query(ctx context.Context, req *pb.QueryRequest) (*pb.QueryResponse, error) {
 	var (
 		value       model.Value
-		warn        promv1.Warnings
+		warn        promv1cli.Warnings
 		err         error
 		metricsData *pb.MetricsData
 	)
-	st := time.Unix(req.GetSt(), 0)
-	et := time.Unix(req.GetEt(), 0)
+	st := time.UnixMilli(req.GetSt())
+	et := time.UnixMilli(req.GetEt())
 	if req.GetStep() == "" {
 		value, warn, err = s.pAPI.Query(ctx, req.GetQuery(), et)
 		if warn != nil {
@@ -91,7 +103,7 @@ func (s *PrometheusService) Query(ctx context.Context, req *pb.QueryRequest) (*p
 			log.Errorf("time step parse err: %s", err1)
 			return nil, pb.ResourceErrUnknown()
 		}
-		value, warn, err = s.pAPI.QueryRange(ctx, req.GetQuery(), promv1.Range{
+		value, warn, err = s.pAPI.QueryRange(ctx, req.GetQuery(), promv1cli.Range{
 			Start: st,
 			End:   et,
 			Step:  step,
@@ -112,4 +124,12 @@ func (s *PrometheusService) Query(ctx context.Context, req *pb.QueryRequest) (*p
 	return &pb.QueryResponse{
 		Result: metricsData,
 	}, nil
+}
+
+func (s *PrometheusService) Rules(ctx context.Context, req *pb.QueryRequest) (*pb.QueryResponse, error) {
+	s.monV1Cli.PrometheusRules(s.TKeelNS)
+	s.monV1Cli.Alertmanagers(s.TKeelNS)
+	s.monV1Alp1Cli.AlertmanagerConfigs(s.TKeelNS)
+
+	return nil, nil
 }
